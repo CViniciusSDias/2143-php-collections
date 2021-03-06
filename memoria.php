df63129e0d0c05e064655f2869b1478a88d9a3a9
Vinicius Dias <vinicius.dias@achievers.com>
2021-03-06 01:32:21 -0300

    Video 3.1

--- memoria.php
+++ memoria.php
@@ -1,9 +1,9 @@
 <?php
 
-$array = new SplFixedArray(32769);
+$array = new SplDoublyLinkedList();
 
 for ($i = 0; $i < 32769; $i++) {
-    $array[$i] = $i;
+    $array->add(3, $i);
 }
 
 var_dump(memory_get_usage() / 1024 / 1024);
