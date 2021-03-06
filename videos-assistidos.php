2ebfd5d77343e445a2c5b282753f97336b422c0c
Vinicius Dias <vinicius.dias@achievers.com>
2021-03-06 13:27:21 -0300

    Video 5.2

--- videos-assistidos.php
+++ videos-assistidos.php
@@ -5,9 +5,12 @@ require_once 'Video.php';
 $video1 = new Video('Aprendendo mapas');
 $video2 = new Video('Aprendendo conjuntos');
 
-$videosAssistidos = [
-    spl_object_hash($video1) => new DateTimeImmutable('2021-02-03'),
-    spl_object_hash($video2) => new DateTimeImmutable('2021-02-01'),
-];
+$videosAssistidos = new SplObjectStorage();
+$videosAssistidos[$video1] = new DateTimeImmutable('2021-02-03');
+$videosAssistidos->attach($video2, new DateTimeImmutable('2021-02-01'));
 
-echo $videosAssistidos[spl_object_hash($video1)]->format('d/m/Y') . PHP_EOL;
+echo $videosAssistidos[$video1]->format('d/m/Y') . PHP_EOL;
+
+foreach ($videosAssistidos as $video) {
+    echo $videosAssistidos[$video]->format('d/m/Y');
+}
